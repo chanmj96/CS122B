68d56bfc260693f75d59603ace3ba91fe8c39b3d
Montblac <montblac95@gmail.com>
2018-01-19 22:22:46 -0800

    Cleaned everything.

--- project1/src/MovieList.java
+++ project1/src/MovieList.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.net.*;
 import java.sql.*;
-import java.text.*;
-import java.util.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
