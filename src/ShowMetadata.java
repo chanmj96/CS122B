52c04055746b1f0f2738110f2eb27d131d8a1686
Montblac <montblac95@gmail.com>
2018-02-20 18:53:09 -0800

    Implemented metadata retrieval

---
+++ src/ShowMetadata.java
@@ -0,0 +1,114 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * Servlet implementation class ShowMetadata
+ */
+@WebServlet("/ShowMetadata")
+public class ShowMetadata extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowMetadata() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String loginUser = "testuser";
+		String loginPasswd = "password";
+		String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
+		
+		response.setContentType("application/json");
+		
+		PrintWriter out = response.getWriter();
+		try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection dbcon = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
+            Statement statement = dbcon.createStatement();
+            
+            String query = "SHOW tables";
+            ResultSet rs = statement.executeQuery(query);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            
+            int cols = rsmd.getColumnCount();
+            JsonObject metadata = new JsonObject();
+            
+            // Gets all table names
+            JsonArray tables = new JsonArray();
+            while(rs.next()) {
+            		for(int i = 1; i <= cols; ++i) {
+            			tables.add(new JsonPrimitive(rs.getString(i).replaceAll("\"", "")));
+            		}
+            }
+            metadata.add("tables", tables);
+            
+            // Gets all columns per table
+            
+            for(JsonElement table : tables) {
+            		String subquery = "SHOW COLUMNS FROM " + table.toString().replaceAll("\"", "");
+            		
+            		ResultSet subrs = statement.executeQuery(subquery);            		
+            		JsonObject attributes = new JsonObject();
+            		
+            		
+            		while(subrs.next()) {
+            			attributes.addProperty(subrs.getString(1), subrs.getString(2));
+            		}
+            		
+            		metadata.add(table.toString().replaceAll("\"", ""), attributes);
+            		subrs.close();
+            }
+            
+            out.write(metadata.toString());
+            
+            rs.close();
+            statement.close();
+            	dbcon.close();
+		} catch (SQLException ex) {
+	            while (ex != null) {
+	                System.out.println("SQL Exception:  " + ex.getMessage());
+	                ex = ex.getNextException();
+	            }
+		} catch (java.lang.Exception ex) {
+			out.println("<HTML>" + "<HEAD><TITLE>" + "MovieDB: Error" + "</TITLE></HEAD>\n<BODY>"
+					+ "<P>SQL error in doGet: " + ex.getMessage() + "</P></BODY></HTML>");
+			return;
+        }
+		out.close();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
