8b9f0b80c263fe040f758a49d152fbce339a6901
Matthew Chan <chanmj2@uci.edu>
2018-02-21 21:33:03 -0800

    AddMovie java file added

---
+++ src/AddMovie.java
@@ -0,0 +1,116 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.CallableStatement;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class AddMovie
+ */
+@WebServlet("/AddMovie")
+public class AddMovie extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection dbcon;
+	private Statement stmt;
+	private ResultSet rs;   
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddMovie() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String loginUser = "testuser"; 
+		String loginPasswd = "password";
+		String loginUrl = "jdbc:mysql://localhost:3306/moviedb?autoReconnect=true&useSSL=false";
+		
+		response.setContentType("application/json");
+		// this example only allows username/password to be test/test
+		// in the real project, you should talk to the database to verify username/password
+		
+		PrintWriter out = response.getWriter();
+		try {
+			String title = request.getParameter("title");
+			String year = request.getParameter("year");
+			String director = request.getParameter("director");
+			String star = request.getParameter("star");
+			String genre = request.getParameter("genre");
+			
+			if(title == null || year == null || director == null)
+			{
+				out.println("Error: movie fields not filled out.");
+				out.close();
+				return;
+			}
+			if(star == null)
+			{
+				out.println("Error: provide a star name to add movie.");
+				out.close();
+				return;
+			}
+			if(genre == null)
+			{
+				out.println("Error: provide a genre name to add movie.");
+				out.close();
+				return;
+			}
+			
+			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+			dbcon = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
+			stmt = dbcon.createStatement();
+			
+			CallableStatement cStmt = dbcon.prepareCall("{call add_movie(?, ?, ?, ?, ?)}");
+			cStmt.setString(1, title);
+			cStmt.setString(2, year);
+			cStmt.setString(3, director);
+			cStmt.setString(4, star);
+			cStmt.setString(5, genre);
+			cStmt.executeUpdate();
+		}
+		catch (SQLException ex) {
+            while (ex != null) {
+                System.out.println("SQL Exception:  " + ex.getMessage());
+                ex = ex.getNextException();
+            }
+		} catch (java.lang.Exception ex) {
+			out.println("<HTML>" + "<HEAD><TITLE>" + "MovieDB: Error" + "</TITLE></HEAD>\n<BODY>"
+					+ "<P>SQL error in doGet: " + ex.getMessage() + "</P></BODY></HTML>");
+			return;
+		}
+		finally {
+			try {stmt.close();} catch (Exception e) {}
+			try {dbcon.close();} catch (Exception e) {}
+			out.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
