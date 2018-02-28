c71f2db6fc788eddb97d4e77666ff1092d21a409
Montblac <montblac95@gmail.com>
2018-02-28 10:45:18 -0800

    Updated script to add indexes on existing tables

--- SqlScripts/create_ft.sql
+++ SqlScripts/create_ft.sql
@@ -1,17 +1,3 @@
 USE moviedb;
-DROP TABLE IF EXISTS ft;
-
-CREATE TABLE ft (
-	id varchar(10),
-	entry varchar(100),
-	PRIMARY KEY (id),
-	FULLTEXT(entry)
-) ENGINE=MyISAM;
-
-INSERT INTO ft (id, entry)
-SELECT id, title
-FROM movies;
-
-INSERT INTO ft (id, entry)
-SELECT id, name
-FROM stars;
+ALTER TABLE movies ADD FULLTEXT INDEX m_entry (title);
+ALTER TABLE stars ADD FULLTEXT INDEX s_entry (name);
